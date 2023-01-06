5ac208a681dba5ba0557e2fb4605931b440d8b1a
RKlein SNow Trial <122062634+RKleinSnowTrial@users.noreply.github.com>
2023-01-06 09:24:25 -0700

    Update streamlit_app.py
    Doing as I'm told like a good wabbit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 bullshit')
+streamlit.text('Kale, Spinach, and Rocket Smoothy')
+streamlit.test('Hard Boild, Free-Range Eggs')
