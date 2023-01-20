7b5657f1754a81162993814b74beba58b792b9b3
RKlein SNow Trial <122062634+RKleinSnowTrial@users.noreply.github.com>
2023-01-19 21:53:11 -0700

    Create streamlit_appp.py

---
+++ streamlit_appp.py
@@ -0,0 +1,37 @@
+import streamlit
+
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 Blue Berry Oatmeal')
+streamlit.text('🥗 Kale, Spinach, and Rocket Smoothy')
+streamlit.text('🐔 Hard Boild, Free-Range Eggs')
+streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+
+streamlit.dataframe(fruits_to_show)
+
+
+streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+
+# normalize json Text response. 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# put it in a table and make it look purty
+streamlit.dataframe(fruityvice_normalized)
+
+# Display the table on the page.
