ce6817eefdf53b4f958a6b66a317c72bcdb55956
RKlein SNow Trial <122062634+RKleinSnowTrial@users.noreply.github.com>
2023-01-20 11:59:58 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-
+# Display the table on the page.
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
@@ -36,3 +36,12 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # put it in a table and make it look purty
 streamlit.dataframe(fruityvice_normalized)
 
+
+# Connect to snowflake directly
+import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
